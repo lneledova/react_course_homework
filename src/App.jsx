fc67d2f6efedb02def5b1742e6200e86ce8276c2
elizaveta neledova <neledova.ev@phystech.edu>
2022-09-24 23:33:21 +0300

    rename from .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import data from './assets/mock-data.json'
 
 import {MakeCard} from "./components/make-card/make-card";
 
-// Hook import for state
-import React from "react";
-
 function App() {
 
   return (
